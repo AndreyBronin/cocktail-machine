86f4202fb6755fb96942a8304ea7f6e6ef8c4617
jonnib <jonnib@yandex.ru>
2018-12-01 03:30:22 -0500

    from codeeniware

--- backend/main.go
+++ backend/main.go
@@ -8,11 +8,26 @@ type Ingredient struct {
   Name string
 }
 
+type Recipe struct {
+  Ingredients map[string]uint8
+}
+
+func NewRecipe() Recipe {
+  return Recipe{make(map[string]uint8, 0)}
+}
+
+func(r *Recipe) AddIngredient(i Ingredient, count uint8) {
+  r.Ingredients[i.Name] = count
+}
 
 func main(){
   coke := Ingredient{"Coca-cola"}
   rum := Ingredient{"Rum"}
-  whisky := Ingredient{"Whisky"}
+  //whisky := Ingredient{"Whisky"}
+  
+  recipe := NewRecipe()
+  recipe.AddIngredient(coke, 100)
+  recipe.AddIngredient(rum, 30)
 
-  fmt.Println(coke, rum, whisky)
+  fmt.Println(recipe)
 }
