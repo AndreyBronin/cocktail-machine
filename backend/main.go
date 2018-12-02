6473dadccd426cbd7a7060c5c85f19d473514faa
vany <ivan@serezhkin.com>
2018-12-02 16:24:20 +0300

    add package blockchain

--- backend/main.go
+++ backend/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type Ingredient struct {
-  Name string
+	Name string
 }
 
 type Recipe struct {
-  Ingredients map[string]uint8
+	Ingredients map[string]uint8
 }
 
 func NewRecipe() Recipe {
-  return Recipe{make(map[string]uint8, 0)}
+	return Recipe{make(map[string]uint8, 0)}
 }
 
-func(r *Recipe) AddIngredient(i Ingredient, count uint8) {
-  r.Ingredients[i.Name] = count
+func (r *Recipe) AddIngredient(i Ingredient, count uint8) {
+	r.Ingredients[i.Name] = count
 }
 
-func main(){
-  coke := Ingredient{"Coca-cola"}
-  rum := Ingredient{"Rum"}
-  //whisky := Ingredient{"Whisky"}
-  
-  recipe := NewRecipe()
-  recipe.AddIngredient(coke, 100)
-  recipe.AddIngredient(rum, 30)
+func main() {
+	coke := Ingredient{"Coca-cola"}
+	rum := Ingredient{"Rum"}
+	//whisky := Ingredient{"Whisky"}
 
-  fmt.Println(recipe)
+	recipe := NewRecipe()
+	recipe.AddIngredient(coke, 100)
+	recipe.AddIngredient(rum, 30)
+
+	fmt.Println(recipe)
 }
