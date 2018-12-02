b1cb58feb05b9651e0e67a71a79d97143c6e39de
vany <ivan@serezhkin.com>
2018-12-02 17:57:23 +0300

    blockchain api

--- backend/blockachain/blockchain.go
+++ backend/blockachain/blockchain.go
@@ -1,5 +1,9 @@
 // Blockchain
 package blockachain
 
+import "github.com/AndreyBronin/cocktail-machine/backend/coctalhal"
+
+// BlockChain interface to public blockchain SDK
 type BlockChain interface {
+	CheckCoctail(contract string) coctalhal.Contains
 }
--- backend/webapi/webapi.go
+++ backend/webapi/webapi.go
@@ -1,5 +1,23 @@
 package webapi
 
+type Ingredient struct {
+	Name string
+}
+
+type Recipe struct {
+	Ingredients map[string]uint8
+}
+
 // WebApi is backend REST API
 type WebApi interface {
+	MakeCocktail(contract string)
+}
+
+type WebAPIComponent struct {
+	Port int
+	Host int
+}
+
+func (wa WebAPIComponent) Start() {
+
 }
